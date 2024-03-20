1e3892f6eec11bdd097e5d434b13040f400fb349
OUYANGRUILONG <871972670@qq.com>
2024-03-20 21:05:32 +0800

    ouy

--- norm_client.py
+++ norm_client.py
@@ -1,69 +1,72 @@
-
 import models, torch, copy
 import torch.nn as nn
 import torch.optim as optim
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+
 class Norm_Client(object):
 
-	def __init__(self, conf, model, train_dataset, id = -1):
-		
-		self.conf = conf
-		
-		self.local_model = models.get_model(self.conf["model_name"])
-		self.local_model = self.local_model.to(device)
+    def __init__(self, conf, model, train_dataset, id=-1):
 
-		# Check GPU availability and move the model to GPU if available
-		# if torch.cuda.is_available():
-		# 	self.local_model = self.local_model.cuda()
-		# self.mask = {}
-		# for name, param in self.local_model.state_dict().items():
-		# 	p = torch.ones_like(param) * self.conf["prop"]
-		# 	if torch.is_floating_point(param):
-		# 		self.mask[name] = torch.bernoulli(p)
-		# 	else:
-		# 		self.mask[name] = torch.bernoulli(p).long()
+        self.conf = conf
 
-		self.client_id = id
-		
-		self.train_dataset = train_dataset
-		
-		all_range = list(range(len(self.train_dataset)))
-		data_len = int(len(self.train_dataset) / self.conf['no_models'])
-		train_indices = all_range[id * data_len: (id + 1) * data_len]
+        self.local_model = model
+        self.local_model = self.local_model.to(device)
 
-		self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=conf["batch_size"], sampler=torch.utils.data.sampler.SubsetRandomSampler(train_indices))
-									
-		
-	def local_train(self, model):
+        # Check GPU availability and move the model to GPU if available
+        # if torch.cuda.is_available():
+        # 	self.local_model = self.local_model.cuda()
+        # self.mask = {}
+        # for name, param in self.local_model.state_dict().items():
+        # 	p = torch.ones_like(param) * self.conf["prop"]
+        # 	if torch.is_floating_point(param):
+        # 		self.mask[name] = torch.bernoulli(p)
+        # 	else:
+        # 		self.mask[name] = torch.bernoulli(p).long()
 
-		for name, param in model.state_dict().items():
-			self.local_model.state_dict()[name].copy_(param.clone())
-	
-		#print(id(model))
-		optimizer = torch.optim.SGD(self.local_model.parameters(), lr=self.conf['lr'],momentum=self.conf['momentum'])
-		#print(id(self.local_model))
-		self.local_model.train()
-		for e in range(self.conf["local_epochs"]):
-			
-			for batch_id, batch in enumerate(self.train_loader):
-				data, target = batch
-				
-				if torch.cuda.is_available():
-					data = data.cuda()
-					target = target.cuda()
-			
-				optimizer.zero_grad()
-				output = self.local_model(data)
-				loss = torch.nn.functional.cross_entropy(output, target)
-				loss.backward()
-			
-				optimizer.step()
-			print("Epoch %d done." % e)	
-		diff = dict()
-		for name, data in self.local_model.state_dict().items():
-			diff[name] = (data - model.state_dict()[name])
-		# print(diff[name])
+        self.client_id = id
 
-		return diff
+        self.train_dataset = train_dataset
 
-		
+        all_range = list(range(len(self.train_dataset)))
+        data_len = int(len(self.train_dataset) / self.conf['no_models'])
+        train_indices = all_range[id * data_len: (id + 1) * data_len]
+
+        self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=conf["batch_size"],
+                                                        sampler=torch.utils.data.sampler.SubsetRandomSampler(
+                                                            train_indices))
+
+    def local_train(self, model):
+
+        for name, param in model.state_dict().items():
+            self.local_model.state_dict()[name].copy_(param.clone())
+
+        # print(id(model))
+        optimizer = torch.optim.SGD(self.local_model.parameters(), lr=self.conf['lr'], momentum=self.conf['momentum'])
+        # print(id(self.local_model))
+        self.local_model.train()
+        for e in range(self.conf["local_epochs"]):
+
+            for batch_id, batch in enumerate(self.train_loader):
+                data, target = batch
+
+                if torch.cuda.is_available():
+                    data = data.cuda()
+                    target = target.cuda()
+
+                optimizer.zero_grad()
+                output = self.local_model(data)
+                loss = torch.nn.functional.cross_entropy(output, target)
+                loss.backward()
+
+                optimizer.step()
+            print("Epoch %d done." % e)
+        diff = dict()
+        for name, data in self.local_model.state_dict().items():
+            diff[name] = (data - model.state_dict()[name])
+        # print(diff[name])
+
+        return diff
+
+
